d1158094865f801ba38eda09b374571f76f8ce13
Cristiane Silva <cristianeavelarmaia@gmail.com>
2018-06-05 19:00:41 +0000

    Fixed reverse errors

--- reverse.js
+++ reverse.js
@@ -4,5 +4,7 @@ if (input)
   console.log(reverse(input));
 
 function reverse(original) {
-  return orignal.split('').reverseList().join('');
+  //typo
+  return original.split('').reverse().join('');
+  //method is reverse
 }
