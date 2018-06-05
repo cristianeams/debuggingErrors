f0bf1d5b91090bb658afc83a619de6ac95acc519
Cristiane Silva <cristianeavelarmaia@gmail.com>
2018-06-05 18:57:44 +0000

    Fixed average errors

--- average.js
+++ average.js
@@ -2,9 +2,14 @@ function average(list) {
   var sum = 0;
 
   for (var num of list) {
-    sum ++ num;
+    //declare var num
+    var num;
+    // fix ++ to += to increment
+    sum += num;
   }
 
-  return sum / list.length;
+return sum / list.length;
+//close } in function
+}
 
 console.log(average([3, 5, 7]));
